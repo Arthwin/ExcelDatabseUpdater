7b7db537a81a94c9bb231df79feb55e2a129d8b2
Arthwin <dan_olarte@hotmail.com>
2017-03-09 17:42:05 -0500

    Patched release

--- ExcelUpdater/ExcelUpdater.py
+++ ExcelUpdater/ExcelUpdater.py
@@ -7,7 +7,7 @@ def read_xl(path):
         raise FileNotFoundError('Failed to find file ' + path)
     try:
         # Can we read it?
-        return pd.read_excel(path)
+        return pd.read_excel(path).astype('str')
     except pd.parser.CParserError as errorm:
         raise FileNotFoundError('Failed to read file ' + path + ' -e: ' + errorm)
 
@@ -15,7 +15,7 @@ def read_xl(path):
 if __name__ == "__main__":
     try:
         # Read Data
-        files = os.listdir() # List all files in root directory
+        files = os.listdir('data/') # List all files in root directory
         updates = []
         databasen = ''
         for file in files:
@@ -24,11 +24,12 @@ if __name__ == "__main__":
                     databasen = file # The last Excel file containign database is the main database, shouldonly be 1
                 else:
                     updates.append(file) # The rest are files to consider to update
+        
         print('The DataBase found is: ' + databasen + '.') # Make sure the database file is the inteded, else quit
         chk = input('Is this correct? (y/n): ')
         if chk.lower() != 'y':
             raise Exception('Make sure the database you want is the only .xlsx file with "database" in its name.')
-        database = read_xl(databasen)
+        database = read_xl('data/' + databasen)
 
         # Update database with each otehr file
         for upfile in updates:
@@ -36,7 +37,7 @@ if __name__ == "__main__":
             chk = input('Do you want to update the DataBase with ' + upfile + '? (y/n): ')
             if chk.lower() == 'y':
                 try:
-                    update = read_xl(upfile)
+                    update = read_xl('data/' + upfile)
 
                     # Attach new columns from update to database
                     for field in list(update):
@@ -62,15 +63,17 @@ if __name__ == "__main__":
                     print('Error: ' + e)
         print('Finished updating!')
         # Save Updated Dataset
+        print('Saving Database ...')
+        database = database.replace({'nan': ''})
         new_file = False
         filei = 0
         while not new_file: # Make sure the new DB is being saved to a new file to prevent errors.
-            if os.path.isfile('updated_'+str(filei)+'.xlsx'):
+            if os.path.isfile('data/updated_'+str(filei)+'.xlsx'):
                 filei += 1
             else:
                 new_file = True
-        new_name = 'updated_'+str(filei)+'.xlsx'
+        new_name = 'data/updated_'+str(filei)+'.xlsx'
         database.to_excel(new_name, index=False)
-        print('New DataBase saved to ' + new_name)
+        input('New DataBase saved to ' + new_name)
     except Exception as e:
         print(e)
